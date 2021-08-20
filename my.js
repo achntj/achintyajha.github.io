71ae0e162da7c51efc5439907c4860163555d70d
achintyajha <achintyakjha@gmail.com>
2021-08-20 09:12:54 +0530

    jquery

--- my.js
+++ my.js
@@ -1,37 +1,21 @@
-$(document).ready(function () {
-  // I only have one form on the page but you can be more specific if need be.
-  var $form = $("form");
-
-  if ($form.length > 0) {
-    $('form input[type="submit"]').bind("click", function (event) {
-      if (event) event.preventDefault();
-      // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
-      if (validate_input($form)) {
-        register($form);
-      }
-    });
-  }
-});
-
-function register($form) {
+$(".myform").submit(function (e) {
+  var $this = $(this);
   $.ajax({
-    type: $form.attr("method"),
-    url: $form.attr("action"),
-    data: $form.serialize(),
-    cache: false,
+    type: "GET", // GET & url for json slightly different
+    url: "https://github.us6.list-manage.com/subscribe/post-json?u=17a470b4b0b42c5f91f52991b&amp;id=ba76bd2ad4&c=?",
+    data: $this.serialize(),
     dataType: "json",
     contentType: "application/json; charset=utf-8",
     error: function (err) {
-      alert(
-        "Could not connect to the registration server. Please try again later."
-      );
+      alert("Could not connect to the registration server.");
     },
     success: function (data) {
       if (data.result != "success") {
-        // Something went wrong, do something to notify the user. maybe alert(data.msg);
+        // Something went wrong, parse data.msg string and display message
       } else {
-        // It worked, carry on...
+        // It worked, so hide form and display thank-you message.
       }
     },
   });
-}
+  return false;
+});
