6a4d4212f2821fc4d62fac98d9c6e863027724b7
achintyajha <achintyakjha@gmail.com>
2021-08-20 09:01:58 +0530

    jquery

--- my.js
+++ my.js
@@ -1,21 +1,37 @@
-$(".myform").submit(function (e) {
-  var $this = $(this);
+$(document).ready(function () {
+  // I only have one form on the page but you can be more specific if need be.
+  var $form = $("form");
+
+  if ($form.length > 0) {
+    $('form input[type="submit"]').bind("click", function (event) {
+      if (event) event.preventDefault();
+      // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
+      if (validate_input($form)) {
+        register($form);
+      }
+    });
+  }
+});
+
+function register($form) {
   $.ajax({
-    type: "GET", // GET & url for json slightly different
-    url: "http://XXXXXXXX.list-manage2.com/subscribe/post-json?c=?",
-    data: $this.serialize(),
+    type: $form.attr("method"),
+    url: $form.attr("action"),
+    data: $form.serialize(),
+    cache: false,
     dataType: "json",
     contentType: "application/json; charset=utf-8",
     error: function (err) {
-      alert("Could not connect to the registration server.");
+      alert(
+        "Could not connect to the registration server. Please try again later."
+      );
     },
     success: function (data) {
       if (data.result != "success") {
-        // Something went wrong, parse data.msg string and display message
+        // Something went wrong, do something to notify the user. maybe alert(data.msg);
       } else {
-        // It worked, so hide form and display thank-you message.
+        // It worked, carry on...
       }
     },
   });
-  return false;
-});
+}
